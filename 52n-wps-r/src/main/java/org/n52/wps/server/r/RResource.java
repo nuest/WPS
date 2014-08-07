73b5184dd69467e09cedf363428e23429285b512
Daniel Nüst <d.nuest@52north.org>
2014-08-07 17:24:06 +0200

    r resource works using annotations

--- 52n-wps-r/src/main/java/org/n52/wps/server/r/RResource.java
+++ 52n-wps-r/src/main/java/org/n52/wps/server/r/RResource.java
@@ -29,22 +29,21 @@
 
 package org.n52.wps.server.r;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-
 import org.n52.wps.server.r.workspace.RSessionManager;
 import org.rosuda.REngine.REXPMismatchException;
 import org.rosuda.REngine.Rserve.RserveException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@Path("/r")
 @Component
+@RequestMapping("/r")
 public class RResource {
 
     protected static Logger log = LoggerFactory.getLogger(RResource.class);
@@ -57,33 +56,35 @@ public class RResource {
         log.debug("NEW {}", this);
     }
 
-    @GET
-    @Path("/resource")
-    public String getResource() {
-        return "Hello resource";
+    @RequestMapping(value = "/resource", method = RequestMethod.GET)
+    public ResponseEntity<String> getResource() {
+        ResponseEntity<String> entity = new ResponseEntity<String>("Hello resource", HttpStatus.OK);
+        return entity;
     }
 
-    @GET
-    @Path("/script")
-    public String getScript() {
-        return "Hello script";
+    @RequestMapping(value = "/script", method = RequestMethod.GET)
+    public ResponseEntity<String> getScript() {
+        ResponseEntity<String> entity = new ResponseEntity<String>("Hello script", HttpStatus.OK);
+        return entity;
     }
 
-    @GET
-    @Path("/sessionInfo")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response sessionInfo() {
+    @RequestMapping(value = "/sessionInfo", produces = MediaType.TEXT_PLAIN_VALUE)
+    public HttpEntity<String> sessionInfo() {
         FilteredRConnection rCon = null;
         try {
             rCon = config.openRConnection();
 
             RSessionManager session = new RSessionManager(rCon, config);
             String sessionInfo = session.getSessionInfo();
-            return Response.ok(sessionInfo).build();
+
+            ResponseEntity<String> entity = new ResponseEntity<String>(sessionInfo, HttpStatus.OK);
+            return entity;
         }
         catch (RserveException | REXPMismatchException e) {
             log.error("Could not open connection to retrieve sesion information.", e);
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+            ResponseEntity<String> entity = new ResponseEntity<String>("R exception: " + e.getMessage(),
+                                                                       HttpStatus.INTERNAL_SERVER_ERROR);
+            return entity;
         }
         finally {
             if (rCon != null)
