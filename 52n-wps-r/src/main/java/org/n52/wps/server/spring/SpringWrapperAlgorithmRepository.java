8869a4f3f93e04ed6a74ab0c5ac5d661e45dba3d
Daniel Nüst <d.nuest@52north.org>
2014-08-13 10:29:07 +0200

    add abstract wrapper repository

--- 52n-wps-r/src/main/java/org/n52/wps/server/spring/SpringWrapperAlgorithmRepository.java
+++ 52n-wps-r/src/main/java/org/n52/wps/server/spring/SpringWrapperAlgorithmRepository.java
@@ -29,15 +29,6 @@
 
 package org.n52.wps.server.spring;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import net.opengis.wps.x100.ProcessDescriptionType;
-
-import org.n52.wps.PropertyDocument.Property;
-import org.n52.wps.RepositoryDocument.Repository;
-import org.n52.wps.commons.WPSConfig;
-import org.n52.wps.server.IAlgorithm;
 import org.n52.wps.server.ITransactionalAlgorithmRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,146 +38,45 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
- * a wrapper class for {@link ITransactionalAlgorithmRepository}, which is managed by spring b
+ * 
+ * A wrapper for @ITransactionalAlgorithmRepository which are managed by Spring.
  * 
  * @author Daniel Nüst
  *
  */
 @Component
-public class SpringWrapperAlgorithmRepository implements ITransactionalAlgorithmRepository, ApplicationContextAware {
+public class SpringWrapperAlgorithmRepository extends AbstractWrapperAlgorithmRepository implements
+        ApplicationContextAware {
 
     private static Logger LOGGER = LoggerFactory.getLogger(SpringWrapperAlgorithmRepository.class);
-
-    public static final String WRAPPEE_NAME = "wrappeeName";
-
-    private static ITransactionalAlgorithmRepository wrappedRepository = null;
 
     public SpringWrapperAlgorithmRepository() {
         LOGGER.info("NEW {}", this);
     }
 
     @Override
-    public Collection<String> getAlgorithmNames() {
-        if (wrappedRepository != null)
-            return wrappedRepository.getAlgorithmNames();
-
-        return new ArrayList<String>();
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("ClassicRAlgorithmRepository [");
-        if (wrappedRepository != null)
-            builder.append("wrappedRepository=").append(wrappedRepository);
-        builder.append("]");
-        return builder.toString();
-    }
-
-    @Override
-    public IAlgorithm getAlgorithm(String processID) {
-        if (wrappedRepository != null)
-            return wrappedRepository.getAlgorithm(processID);
-
-        return null;
-    }
-
-    @Override
-    public ProcessDescriptionType getProcessDescription(String processID) {
-        if (wrappedRepository != null)
-            return wrappedRepository.getProcessDescription(processID);
-
-        return null;
-    }
-
-    @Override
-    public boolean containsAlgorithm(String processID) {
-        if (wrappedRepository != null)
-            return wrappedRepository.containsAlgorithm(processID);
-
-        return false;
-    }
-
-    @Override
-    public void shutdown() {
-        if (wrappedRepository != null)
-            wrappedRepository.shutdown();
-    }
-
-    @Override
-    public boolean addAlgorithm(Object processID) {
-        if (wrappedRepository != null)
-            return wrappedRepository.addAlgorithm(processID);
-
-        return false;
-    }
-
-    @Override
-    public boolean removeAlgorithm(Object processID) {
-        if (wrappedRepository != null)
-            return wrappedRepository.removeAlgorithm(processID);
-
-        return false;
-    }
-
-    @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         LOGGER.debug("Receiving application context: {}", applicationContext);
 
-        String className = this.getClass().getCanonicalName();
-        WPSConfig conf = WPSConfig.getInstance();
-        if ( !conf.isRepositoryActive(className)) {
-            LOGGER.debug("{} is inactive.", className);
+        if ( !isActive())
             return;
-        }
 
-        String wrappeeName = null;
-        Property[] properties = conf.getPropertiesForRepositoryClass(className);
-        for (Property property : properties) {
-            if (property.getActive() && property.getName().equals(WRAPPEE_NAME))
-                wrappeeName = property.getStringValue();
-        }
-        LOGGER.info("Wrapping {}", wrappeeName);
-        
         try {
-            Class< ? > repositoryClass = SpringWrapperAlgorithmRepository.class.getClassLoader().loadClass(wrappeeName);
+            Class< ? > repositoryClass = getWrappeeClass();
             Component componentAnnotation = repositoryClass.getAnnotation(Component.class);
             if (componentAnnotation != null) {
                 String componentName = componentAnnotation.value();
                 Object bean = applicationContext.getBean(componentName);
                 if (bean instanceof ITransactionalAlgorithmRepository) {
-                    wrappedRepository = (ITransactionalAlgorithmRepository) bean;
-                    LOGGER.debug("Loaded wrapped repository from context: {}", wrappedRepository);
+                    ITransactionalAlgorithmRepository repo = (ITransactionalAlgorithmRepository) bean;
+                    LOGGER.debug("Loaded repository from context: {}", repo);
+                    setWrappedRepository(repo);
                 }
             }
         }
         catch (ClassNotFoundException e) {
-            LOGGER.error("Problem wrapping algorithm repository with name {}", wrappeeName, e);
+            LOGGER.error("Problem wrapping algorithm repository with name '{}'", getWrappeeName(), e);
         }
-    }
-
-    public static boolean wrapperRepositoryActiveAndConfiguredForRepo(ITransactionalAlgorithmRepository repo,
-                                                                      Repository[] registeredAlgorithmRepositories) {
-        for (Repository repository : registeredAlgorithmRepositories) {
-            if (repository.getActive()) {
-                Property[] propertyArray = repository.getPropertyArray();
-                for (Property property : propertyArray) {
-                    if (property.getActive()
-                            && property.getName().equals(SpringWrapperAlgorithmRepository.WRAPPEE_NAME)) {
-                        String propValue = property.getStringValue();
-                        String className = repo.getClass().getName();
-                        if (propValue.equals(className)) {
-                            LOGGER.info("Wrapper for {} found: {} (class: {})",
-                                         className,
-                                         repository.getName(),
-                                         repository.getClass());
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-        return false;
     }
 
 }
